bcdcfe736843a3402bbf4be7750f65126e257466
Florian Dreyer <flodreyer@web.de>
2023-02-13 16:21:30 +0100

    fixed spacing and col size for smaller screens

--- src/components/RecentCard.jsx
+++ src/components/RecentCard.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 
 const RecentCard = (props) => {
     return (
-        <Col xs={12} md={3}>
+        <Col xs={12} sm={6} md={3}>
             <Link to={`/${props.rec.loc}`}>
-                <div className="d-flex flex-column recent-card">
+                <div className="d-flex flex-column recent-card mb-3">
                     <h5>{props.rec.loc}</h5>
                     <div className="d-flex align-items-center">
                         <img src={`http://openweathermap.org/img/wn/${props.rec.icon}@2x.png`} alt={props.rec.temp}/>
